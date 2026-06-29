edc01294dfc4f5d063d1397e0d5975788db407e0
José Vargas <tu-correo@ejemplo.com>
2026-06-29 16:50:58 -0500

    fix: cambiar SMTP a puerto 587

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -4,8 +4,8 @@ dotenv.config()
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.USER_MAILTRAP,
         pass: process.env.PASS_MAILTRAP
